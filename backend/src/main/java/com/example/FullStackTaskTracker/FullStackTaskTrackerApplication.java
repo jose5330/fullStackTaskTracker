eb392b7fc880c76863e99de70744fe6ce60903fc
jose5330 <christoj5330@gmail.com>
2025-07-12 12:45:43 +0930

    initial commit

---
+++ backend/src/main/java/com/example/FullStackTaskTracker/FullStackTaskTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.FullStackTaskTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackTaskTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackTaskTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/FullStackTaskTracker/FullStackTaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FullStackTaskTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackTaskTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
